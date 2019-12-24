a43ad098877bd9365f8d708062470110b5f00945
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2019-12-24 09:09:37 +0100

    Committed by Guitaca Publishers

--- ACJB/ACJB.ps1
+++ ACJB/ACJB.ps1
@@ -33,177 +33,187 @@ Function LoginPsPnP()
 	Connect-PnPOnline -Url $configFile.appsettings.spUrl -Credentials $myCredentials
 }
 
-Function ExecuteRestQuery($SiteBaseUrl, $BaseRestQuery, $PostRestQuery, $RequestType)
-{
-    if ($RequestType -eq [TypeRequest]::GET)
-    { return ExecuteRestGetQuery $SiteBaseUrl $BaseRestQuery }
-
-    $SiteRestUrl = $SiteBaseUrl + $BaseRestQuery;
-
-    $myCookies = GetAuthCookies $SiteBaseUrl
-    $myFormDigest = GetFormDigest $SiteBaseUrl $myCookies
-
-    $myWebReq = GetRequest $SiteRestUrl $myCookies $PostRestQuery.Length
-    $myWebReq.Headers.Add("X-RequestDigest", $myFormDigest)
-
-	if($RequestType -eq [TypeRequest]::MERGE) {
-		$myWebReq.Headers.Add("IF-MATCH", "*")
-		$myWebReq.Headers.Add("X-HTTP-Method", "MERGE")
-	}
-	elseif($RequestType -eq [TypeRequest]::DELETE) {
-		$myWebReq.Headers.Add("IF-MATCH", "*")
-		$myWebReq.Headers.Add("X-HTTP-Method", "DELETE")
-	}
-
-    $myReqStream = New-Object System.IO.StreamWriter($myWebReq.GetRequestStream())
-    $myReqStream.Write($PostRestQuery)
-    $myReqStream.Flush()
-
-    $resultJson = GetResult $myWebReq
-    if ($resultJson -ne $null) { 
-		$resultObject = $resultJson | ConvertFrom-Json
-		$resultValue = $resultObject.d
-		
-		return $resultValue
-	}
-    else
-		{ return string.Empty; }
-}
-
-Function ExecuteRestGetQuery($SiteBaseUrl, $BaseRestQuery)
-{
-    $SiteRestUrl = $SiteBaseUrl + $BaseRestQuery
-
-    $myCredentials = GetCredentials
-
-    $myWebClient = New-Object System.Net.WebClient
-    $myWebClient.Headers.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f")
-    $myWebClient.Credentials = $myCredentials
-    $myWebClient.Headers.Add("ContentType", "application/json;odata=verbose")
-    $myWebClient.Headers.Add("Accept", "application/json;odata=verbose")
-
-    $myResult = $myWebClient.DownloadString($SiteRestUrl)
-
-	$resultObject = $myResult | ConvertFrom-Json
-	$resultValue = ConvertTo-Json $resultObject.d
-
-	return $resultValue
-}
-
-Function GetAuthCookies($SiteBaseUrl)
-{
-    $myCredentials = GetCredentials
-
-    $authCookie = $myCredentials.GetAuthenticationCookie($SiteBaseUrl)
-    $myCookies = New-Object System.Net.CookieContainer
-    $myCookies.SetCookies($SiteBaseUrl, $authCookie)
-
-    return $myCookies
-}
-
-Function GetCredentials()
-{
-	[SecureString]$securePW = ConvertTo-SecureString -String `
-		$configFile.appsettings.spUserPw -AsPlainText -Force
-
-	$myCredentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials `
-			($configFile.appsettings.spUserName, $securePW)
-
-    return $myCredentials
-}
-
-Function GetFormDigest($SiteBaseUrl, $Cookies)
-{
-    $resourceUrl = $SiteBaseUrl + "/_api/contextinfo"
-    $myWebReq = GetRequest $resourceUrl $Cookies 0
-
-    $resultJson = GetResult $myWebReq
-
-	$resultObject = $resultJson | ConvertFrom-Json
-	$formDigestValue = $resultObject.d.GetContextWebInformation.FormDigestValue
-
-    return $formDigestValue
-}
-
-Function GetRequest($ReqUrl, $Cookies, $ContentLenght)
-{
-    $myWebReq = [System.Net.WebRequest]::Create($ReqUrl)
-    $myWebReq.CookieContainer = $Cookies
-    $myWebReq.Method = "POST"
-    $myWebReq.Accept = "application/json;odata=verbose"
-    $myWebReq.ContentLength = $ContentLenght
-    $myWebReq.ContentType = "application/json;odata=verbose"
-
-    return $myWebReq
-}
-
-Function GetResult($WebRequest)
-{
-    $myResult = ""
-    $myWebResp = $WebRequest.GetResponse()
-
-	Using-Object ($myRespStream = `
-				New-Object System.IO.StreamReader($myWebResp.GetResponseStream())) {
-		$myResult = $myRespStream.ReadToEnd()
-		return $myResult
-	}
-}
-
-function Using-Object
-{
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [AllowEmptyString()]
-        [AllowEmptyCollection()]
-        [AllowNull()]
-        [Object]
-        $InputObject,
+Function Invoke-RestSPO() {
+	Param (
+		[Parameter(Mandatory=$True)]
+		[String]$Url,
  
-        [Parameter(Mandatory = $true)]
-        [scriptblock]
-        $ScriptBlock
-    )
+		[Parameter(Mandatory=$False)]
+		[Microsoft.PowerShell.Commands.WebRequestMethod]$Method = `
+								[Microsoft.PowerShell.Commands.WebRequestMethod]::Get,
  
-    try {
-        . $ScriptBlock
-    }
-    finally {
-        if ($null -ne $InputObject -and $InputObject -is [System.IDisposable]) {
-            $InputObject.Dispose()
-        }
+		[Parameter(Mandatory=$True)]
+		[String]$UserName,
+ 
+		[Parameter(Mandatory=$False)]
+		[String]$Password,
+ 
+		[Parameter(Mandatory=$False)]
+		[String]$Metadata,
+
+		[Parameter(Mandatory=$False)]
+		[System.Byte[]]$Body,
+ 
+		[Parameter(Mandatory=$False)]
+		[String]$RequestDigest,
+ 
+		[Parameter(Mandatory=$False)]
+		[String]$ETag,
+ 
+		[Parameter(Mandatory=$False)]
+		[String]$XHTTPMethod,
+
+		[Parameter(Mandatory=$False)]
+		[System.String]$Accept = "application/json;odata=verbose",
+
+		[Parameter(Mandatory=$False)]
+		[String]$ContentType = "application/json;odata=verbose",
+
+		[Parameter(Mandatory=$False)]
+		[Boolean]$BinaryStringResponseBody = $False
+	)
+ 
+	if([string]::IsNullOrEmpty($Password)) {
+		$SecurePassword = Read-Host -Prompt "Enter the password" -AsSecureString 
+	}
+	else {
+		$SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+	}
+ 
+	$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials(`
+															$UserName, $SecurePassword)
+	$request = [System.Net.WebRequest]::Create($Url)
+	$request.Credentials = $credentials
+	$request.Headers.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f")
+	$request.ContentType = $ContentType
+	$request.Accept = $Accept
+	$request.Method=$Method
+ 
+	if($RequestDigest) { 
+		$request.Headers.Add("X-RequestDigest", $RequestDigest)
+	}
+	if($ETag) { 
+		$request.Headers.Add("If-Match", $ETag)
+	}
+	if($XHTTPMethod) { 
+		$request.Headers.Add("X-HTTP-Method", $XHTTPMethod)
+	}
+	if($Metadata -or $Body) {
+		if($Metadata) {     
+			$Body = [byte[]][char[]]$Metadata
+		}      
+		$request.ContentLength = $Body.Length 
+		$stream = $request.GetRequestStream()
+		$stream.Write($Body, 0, $Body.Length)
+	}
+	else {
+		$request.ContentLength = 0
+	}
+
+	$response = $request.GetResponse()
+	try {
+		if($BinaryStringResponseBody -eq $False) {    
+			$streamReader = New-Object System.IO.StreamReader $response.GetResponseStream()
+			try {
+				$data=$streamReader.ReadToEnd()
+				$results = $data | ConvertFrom-Json
+				$results.d 
+			}
+			finally {
+				$streamReader.Dispose()
+			}
+		}
+		else {
+			$dataStream = New-Object System.IO.MemoryStream
+			try {
+			Stream-CopyTo -Source $response.GetResponseStream() -Destination $dataStream
+			$dataStream.ToArray()
+			}
+			finally {
+				$dataStream.Dispose()
+			} 
+		}
+	}
+	finally {
+		$response.Dispose()
+	}
+}
+ 
+Function Get-SPOContextInfo(){
+	Param(
+		[Parameter(Mandatory=$True)]
+		[String]$WebUrl,
+ 
+		[Parameter(Mandatory=$True)]
+		[String]$UserName,
+ 
+		[Parameter(Mandatory=$False)]
+		[String]$Password
+	)
+   
+	$Url = $WebUrl + "/_api/contextinfo"
+	Invoke-RestSPO $Url Post $UserName $Password
+}
+
+Function Stream-CopyTo([System.IO.Stream]$Source, [System.IO.Stream]$Destination) {
+    $buffer = New-Object Byte[] 8192 
+    $bytesRead = 0
+    while (($bytesRead = $Source.Read($buffer, 0, $buffer.Length)) -gt 0) {
+         $Destination.Write($buffer, 0, $bytesRead)
     }
 }
-
-enum TypeRequest { GET 
-				   POST 
-				   MERGE 
-				   DELETE }
 
 #-----------------------------------------------------------------------------------------
 
-## Running the Functions
+# Running the Functions
 Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
 Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
 
 [xml]$configFile = get-content "C:\Projects\spPs.values.config"
 
+##==> CSOM
 $spCtx = LoginPsCsom
 $rootWeb = $spCtx.Web
 $spCtx.Load($rootWeb)
 $spCtx.ExecuteQuery()
+$spCtx.Dispose()
 Write-Host $rootWeb.Created  
 
+##==> PSO
 LoginPsPSO
 Test-SPOSite $configFile.appsettings.spUrl
+Disconnect-SPOService
 
-LoginPsPnP
-$myWeb = Get-PnPWeb
-Write-Host $myWeb.Title
-#Get-PnPTenant
+##==> PnP PowerShell
+#LoginPsPnP
+#$myWeb = Get-PnPWeb
+#Write-Host $myWeb.Title
+##Get-PnPTenant
+#Disconnect-PnPOnline  # Doesn't work, it does not disconnect the session, thus it gets an error calling the next routine
 
-$ResutlRest = ExecuteRestQuery $configFile.appsettings.spUrl "/_api/web/Created"
-Write-Host $ResutlRest
+#==> REST
+$webUrl = $configFile.appsettings.spUrl
+$userName = $configFile.appsettings.spUserName
+$password = $configFile.appsettings.spUserPw
+
+## Simple GET request without body
+#$endpointUrl = $WebUrl + "/_api/web/Created"
+#$myResult = Invoke-RestSPO -Url $endpointUrl -Method Get -UserName $userName -Password $password
+#$myResult
+
+## Full POST query with data in the body
+#$endpointUrl = $WebUrl + "/_api/web/lists"
+#$myPayload = @{ 
+#			__metadata = @{ 'type' = 'SP.List' }; 
+#			Title = 'NewListRest'; 
+#			BaseTemplate = 100; 
+#			Description = 'Test NewListRest'; 
+#            AllowContentTypes = $true;
+#            ContentTypesEnabled = $true
+#		   } | ConvertTo-Json
+#$contextInfo = Get-SPOContextInfo -WebUrl $WebUrl -UserName $userName -Password $password
+#Invoke-RestSPO -Url $endpointUrl -Method Post -UserName $userName -Password $password -Metadata $myPayload -RequestDigest $contextInfo.GetContextWebInformation.FormDigestValue 
 
 Write-Host ""  
+
 
