f1111ed6ca34142be70f24c2e50307c4c2ec9d51
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2019-12-24 09:25:27 +0100

    Committed by Guitaca Publishers

---
+++ RBYP/Privacy.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RBYP
+{
+    public partial class Privacy : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
