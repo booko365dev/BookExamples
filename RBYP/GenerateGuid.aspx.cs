71aa12834702e8ac215f15066e3b2a57b02d24c8
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2019-12-24 09:24:51 +0100

    Committed by Guitaca Publishers

---
+++ RBYP/GenerateGuid.aspx.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace RBYP
+{
+    public partial class GenerateGuid : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnGenerateGuid_Click(object sender, EventArgs e)
+        {
+            lblNewGuid.Text = Guid.NewGuid().ToString();
+        }
+    }
+}
