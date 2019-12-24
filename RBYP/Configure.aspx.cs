859084548c0ca52fcb8db22fd7e831ec64c76bdf
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2019-12-24 09:24:40 +0100

    Committed by Guitaca Publishers

---
+++ RBYP/Configure.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RBYP
+{
+    public partial class Configure : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
