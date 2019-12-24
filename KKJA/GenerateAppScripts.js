521d350cd35c7d6a19220f39f81652e0ee9aacff
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2019-12-24 09:23:43 +0100

    Committed by Guitaca Publishers

---
+++ KKJA/GenerateAppScripts.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    microsoftTeams.initialize();
+
+    microsoftTeams.getContext(function (context) {
+        if (context && context.theme) {
+            document.getElementById('lblContextInfo').innerText = context.loginHint;
+            setTheme(context.theme);
+        }
+    });
+
+    microsoftTeams.registerOnThemeChangeHandler(function (theme) {
+        setTheme(theme);
+    });
+
+    function setTheme(theme) {
+        if (theme) {
+        // Possible values for theme: 'default', 'light', 'dark' and 'contrast'
+        document.body.className = 'theme-' + (theme === 'default' ? 'light' : theme);
+        }
+    }
+})();
+
