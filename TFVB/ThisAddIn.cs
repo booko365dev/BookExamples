13ac455e1e307de7c3882a1bfbe5f24d1d8b8943
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2019-12-24 09:16:23 +0100

    Committed by Guitaca Publishers

---
+++ TFVB/ThisAddIn.cs
@@ -0,0 +1,38 @@
+namespace TFVB
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            ShowPanel();
+        }
+
+        private Microsoft.Office.Tools.CustomTaskPane customPanel;
+        public void ShowPanel()
+        {
+            UserControlExcelAddIn panelObject = new UserControlExcelAddIn();
+            customPanel = this.CustomTaskPanes.Add(panelObject, "My Panel");
+            customPanel.Width = panelObject.Width;
+            customPanel.Visible = true;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
+
