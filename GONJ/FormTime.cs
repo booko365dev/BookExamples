02b531baceef073a31002f71e23928df7a33d457
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2019-12-24 09:14:21 +0100

    Committed by Guitaca Publishers

---
+++ GONJ/FormTime.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GONJ
+{
+    public partial class FormTime : Form
+    {
+        public FormTime()
+        {
+            InitializeComponent();
+        }
+
+        private void FormTime_Load(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
+
