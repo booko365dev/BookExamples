fca2ebaed69160c5d0b8cf2829524864e0135c12
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2019-12-24 09:25:42 +0100

    Committed by Guitaca Publishers

---
+++ RBYP/Terms.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RBYP
+{
+    public partial class Terms : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
