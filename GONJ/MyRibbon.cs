048ec8d2b3e61026580a49194d14c6f8e2e1e69c
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2019-12-24 09:14:34 +0100

    Committed by Guitaca Publishers

---
+++ GONJ/MyRibbon.cs
@@ -0,0 +1,59 @@
+using Microsoft.Office.Tools.Ribbon;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace GONJ
+{
+    public partial class MyRibbon
+    {
+        private void MyRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void BtnAddPicture_Click(object sender, RibbonControlEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.ValidateNames = true;
+
+            fileDialog.Filter = "All|*.*|Bitmap|*.bmp|Gif|*.gif|JPEG|*.jpeg|Png|*.png";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Globals.ThisAddIn.Application.ActiveDocument.Shapes.AddPicture(
+                                                            fileDialog.FileName);
+            }
+        }
+
+        private void BtnAddTable_Click(object sender, RibbonControlEventArgs e)
+        {
+            Globals.ThisAddIn.Application.ActiveDocument.Tables.Add(
+                Globals.ThisAddIn.Application.ActiveDocument.Range(0, 0), 3, 4);
+            Globals.ThisAddIn.Application.ActiveDocument.Tables[1].Range.
+                Shading.BackgroundPatternColor = Word.WdColor.wdColorAqua;
+            Globals.ThisAddIn.Application.ActiveDocument.Tables[1].Range.Font.Size = 12;
+            Globals.ThisAddIn.Application.ActiveDocument.Tables[1].Rows.Borders.Enable = 1;
+        }
+
+        private void BtnSaveAsPdf_Click(object sender, RibbonControlEventArgs e)
+        {
+            SaveFileDialog exportDialog = new SaveFileDialog();
+            exportDialog.ValidateNames = true;
+
+            if (exportDialog.ShowDialog() == DialogResult.OK)
+            {
+                Globals.ThisAddIn.Application.ActiveDocument.ExportAsFixedFormat(
+                            exportDialog.FileName,
+                            Word.WdExportFormat.wdExportFormatPDF,
+                            OpenAfterExport: true);
+            }
+        }
+
+        private void BtnGetTime_Click(object sender, RibbonControlEventArgs e)
+        {
+            FormTime newForm = new FormTime();
+            newForm.Show();
+        }
+
+    }
+}
+
