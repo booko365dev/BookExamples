6522e30ecd8bfc6a95a300020ee43f9d15a530d5
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2020-04-01 22:33:12 +0200

    Add files via upload

---
+++ LNTT/LNTTWeb/MessageRead.js
@@ -0,0 +1,30 @@
+﻿//gavdcodebegin 02
+(function () {
+    "use strict";
+
+    Office.initialize = function (reason) {
+    $(document).ready(function () {
+        $('#btnCallRest').text("Call REST Service")
+        $('#btnCallRest').click(CallRest)
+    });
+    };
+
+    function CallRest() {
+        var ServiceUrl = "https://reqres.in/api/users/2"
+
+        CallGetAsync(ServiceUrl).then(
+            function (resultFromService) {
+                var resultString = resultFromService.data.first_name + " " +
+                    resultFromService.data.last_name + " - " +
+                    resultFromService.data.email
+                $('#resultRest').text(resultString);
+            })
+    }
+
+    async function CallGetAsync(urlToCall) {
+        var urlResponse = await fetch(urlToCall)
+        var responseData = await urlResponse.json()
+        return responseData
+    }
+})();
+//gavdcodeend 02
