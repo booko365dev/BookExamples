3e02828a016274b2cf45ca11f34c11283e7ba564
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2020-04-01 22:17:40 +0200

    Add files via upload

---
+++ NBGF/NBGFWeb/MessageRead.js
@@ -0,0 +1,116 @@
+﻿(function () {
+  "use strict";
+
+  var messageBanner;
+
+  // The Office initialize function must be run each time a new page is loaded.
+  //gavdcodebegin 02
+  Office.initialize = function (reason) {
+    $(document).ready(function () {
+
+        $('#btnCreateEmail').text("Create new Email")
+        $('#btnCreateEmail').click(CreateNewEmail)
+
+      var element = document.querySelector('.MessageBanner');
+      messageBanner = new components.MessageBanner(element);
+      messageBanner.hideBanner();
+      loadProps();
+    });
+  };
+  //gavdcodeend 02
+
+    //gavdcodebegin 03
+    function CreateNewEmail() {
+        Office.context.mailbox.displayNewMessageForm(
+            {
+                toRecipients: Office.context.mailbox.item.to,
+                ccRecipients: ['somebody@somewhere.com'],
+                subject: 'New Email created by an Add-in',
+                htmlBody: 'To Whom It May Concern<br/><img src="cid:image.jpg">',
+                attachments: [
+                    {
+                        type: 'file',
+                        name: 'image.jpg',
+                        url: 'http://www.guitaca.com/Logo_01.jpg',
+                        isInline: true
+                    }
+                ]
+            });
+    }
+    //gavdcodeend 03
+
+  // Take an array of AttachmentDetails objects and build a list of attachment names, separated by a line-break.
+  function buildAttachmentsString(attachments) {
+    if (attachments && attachments.length > 0) {
+      var returnString = "";
+      
+      for (var i = 0; i < attachments.length; i++) {
+        if (i > 0) {
+          returnString = returnString + "<br/>";
+        }
+        returnString = returnString + attachments[i].name;
+      }
+
+      return returnString;
+    }
+
+    return "None";
+  }
+
+  // Format an EmailAddressDetails object as
+  // GivenName Surname <emailaddress>
+  function buildEmailAddressString(address) {
+    return address.displayName + " &lt;" + address.emailAddress + "&gt;";
+  }
+
+  // Take an array of EmailAddressDetails objects and
+  // build a list of formatted strings, separated by a line-break
+  function buildEmailAddressesString(addresses) {
+    if (addresses && addresses.length > 0) {
+      var returnString = "";
+
+      for (var i = 0; i < addresses.length; i++) {
+        if (i > 0) {
+          returnString = returnString + "<br/>";
+        }
+        returnString = returnString + buildEmailAddressString(addresses[i]);
+      }
+
+      return returnString;
+    }
+
+    return "None";
+  }
+
+  // Load properties from the Item base object, then load the
+  // message-specific properties.
+  function loadProps() {
+    var item = Office.context.mailbox.item;
+
+    $('#dateTimeCreated').text(item.dateTimeCreated.toLocaleString());
+    $('#dateTimeModified').text(item.dateTimeModified.toLocaleString());
+    $('#itemClass').text(item.itemClass);
+    $('#itemId').text(item.itemId);
+    $('#itemType').text(item.itemType);
+
+    $('#message-props').show();
+
+    $('#attachments').html(buildAttachmentsString(item.attachments));
+    $('#cc').html(buildEmailAddressesString(item.cc));
+    $('#conversationId').text(item.conversationId);
+    $('#from').html(buildEmailAddressString(item.from));
+    $('#internetMessageId').text(item.internetMessageId);
+    $('#normalizedSubject').text(item.normalizedSubject);
+    $('#sender').html(buildEmailAddressString(item.sender));
+    $('#subject').text(item.subject);
+    $('#to').html(buildEmailAddressesString(item.to));
+  }
+
+  // Helper function for displaying notifications
+  function showNotification(header, content) {
+    $("#notificationHeader").text(header);
+    $("#notificationBody").text(content);
+    messageBanner.showBanner();
+    messageBanner.toggleExpansion();
+  }
+})();
