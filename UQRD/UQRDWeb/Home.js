b642958fcf8eb43a20d52718d82af4f2dbc3bc2b
Book Office 365 Developer Recipes <50792214+booko365dev@users.noreply.github.com>
2020-04-01 20:28:08 +0200

    Add files via upload

---
+++ UQRD/UQRDWeb/Home.js
@@ -0,0 +1,50 @@
+﻿//gavdcodebegin 02
+(function () {
+    "use strict"
+
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            $('#btnShowText').text("Copy selected text")
+            $('#btnShowText').click(ShowText)
+
+            WriteSomeText()
+        })
+    }
+
+    function ShowText() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (myResult) {
+                if (myResult.status === Office.AsyncResultStatus.Succeeded) {
+                    WriteToBox("The text is: '" + myResult.value + "'")
+                } else {
+                    WriteToBox("Error: " + myResult.error.message)
+                }
+            })
+    }
+
+    function WriteToBox(TextToWrite) {
+        document.getElementById('myTextArea').innerText += TextToWrite
+    }
+
+    function WriteSomeText() {
+        Word.run(function (context) {
+            var docBody = context.document.body
+            docBody.clear()
+            docBody.insertText(
+                "To Whom It May Concern",
+                Word.InsertLocation.end)
+
+            return context.sync()
+        })
+            .catch(CathMyError)
+    }
+
+    function CathMyError(error) {
+        showNotification("Error:", error)
+        console.log("Error: " + error)
+        if (error instanceof OfficeExtension.Error) {
+            console.log("Debug info: " + JSON.stringify(error.debugInfo))
+        }
+    }
+})()
+//gavdcodeend 02
